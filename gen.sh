53644391ec710eee52c8bea129acf846d15cd77c
Julyfun MacOS13.4 M1 <43675484+julyfun@users.noreply.github.com>
2024-04-22 16:12:27 +0800

    更好的 clone

--- gen.sh
+++ gen.sh
@@ -3,9 +3,9 @@
 folder="ds-gen6"
 path="../$folder"
 mkdir -p $path
-git clone . "$path/Stage 1 - BSTNode BSTMap" -b stage1-v0.1
-git clone . "$path/Stage 2 - Balanced BST" -b stage2-v0.3
-git clone . "$path/Stage 3 - Iterator" # -b stage3-v0.2
+git clone "file://$(pwd)" "$path/Stage 1 - BSTNode BSTMap" -b stage1-v0.1 --depth=1
+git clone "file://$(pwd)" "$path/Stage 2 - Balanced BST" -b stage2-v0.3 --depth=1
+git clone "file://$(pwd)" "$path/Stage 3 - Iterator" --depth=1 # -b stage3-v0.2
 cp References.md README.md README.pdf test-crun.sh $path
 chmod +x $path/test-crun.sh
 cd $path
