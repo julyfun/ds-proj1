b140bb6635377f018cb59a7d8b1bbf4d79b66234
Julyfun MacOS13.4 M1 <43675484+julyfun@users.noreply.github.com>
2024-04-22 16:25:13 +0800

    rm before gen

--- gen.sh
+++ gen.sh
@@ -2,6 +2,8 @@
 # modify the order numbers
 folder="ds-gen6"
 path="../$folder"
+rm -rf $path
+rm "../$folder.zip"
 mkdir -p $path
 git clone "file://$(pwd)" "$path/Stage 1 - BSTNode BSTMap" -b stage1-v0.1 --depth=1
 git clone "file://$(pwd)" "$path/Stage 2 - Balanced BST" -b stage2-v0.3 --depth=1
