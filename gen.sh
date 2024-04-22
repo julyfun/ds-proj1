51a12d0cbe8d3999636bd51027169b8b37b8e589
Julyfun MacOS13.4 M1 <43675484+julyfun@users.noreply.github.com>
2024-04-22 16:04:46 +0800

    ehh

--- gen.sh
+++ gen.sh
@@ -1,11 +1,11 @@
 # genereate a pdf first
 # modify the order numbers
-folder="ds-gen5"
+folder="ds-gen6"
 path="../$folder"
 mkdir -p $path
 git clone . "$path/Stage 1 - BSTNode BSTMap" -b stage1-v0.1
 git clone . "$path/Stage 2 - Balanced BST" -b stage2-v0.3
-git clone . "$path/Stage 3 - Iterator" -b stage3-v0.2
+git clone . "$path/Stage 3 - Iterator" # -b stage3-v0.2
 cp References.md README.md README.pdf test-crun.sh $path
 chmod +x $path/test-crun.sh
 cd $path
